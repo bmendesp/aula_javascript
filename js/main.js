b6e684fcf39e2d0d7e7567f84e13305358f9fec9
Bruno Mendes <brunomvca@gmail.com>
2020-07-02 00:14:02 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Bruno Mendes";
+var idade = 38;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo.";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toLowerCase());
